69044529432b45cc0e0ea20f6fa7f25e0acbc97b
abderrahmanmak <abderrahmanmk55@gmail.com>
2026-08-24 02:13:56 +0100

    wsgi & asgi fix 1

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
 """
 
 import os
+import sys
 
 from django.core.asgi import get_asgi_application
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.config.settings')
 
